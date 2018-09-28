6e70da9b3602ab72b3f6d8593954d803e38e1c31
Unknown <luiszumarraga95@gmail.com>
2018-09-28 12:50:43 +0200

    gsd

--- boletin01act05/app/src/main/java/com/example/lzumarraga/boletin01act05/MainActivity.java
+++ boletin01act05/app/src/main/java/com/example/lzumarraga/boletin01act05/MainActivity.java
@@ -5,17 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int maxTextSize = 50;
     private final int minTextSize = 10;
+    private int idFoto = 0;
+    ImageView img1 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img1 = findViewById(R.id.imageView1);
+        img1.setTag(android.R.drawable.btn_star_big_off);
     }
 
     public void aumentar (View v){
@@ -38,28 +43,37 @@ public class MainActivity extends AppCompatActivity {
         t1.setTextSize(20);
     }
 
+    public void cambiarFoto(View v){
+        /*
+        if(idFoto == 0){
+            img1.setImageResource(android.R.drawable.btn_star_big_off);
+            idFoto = 1;
+        }else{
+            img1.setImageResource(android.R.drawable.btn_star_big_on);
+            idFoto = 0;
+        }*/
+
+        if((Integer)img1.getTag() == android.R.drawable.btn_star_big_off){
+            img1.setImageResource(android.R.drawable.btn_star_big_on);
+        }else{
+            img1.setImageResource(android.R.drawable.btn_star_big_off);
+        }
+    }
+    /*
     public void bucle (View v) throws InterruptedException {
         TextView t1 = findViewById(R.id.text1);
         for(int i = 0; i < 99999; i++){
 
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() + 4);
-            SystemClock.sleep(7000);
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() + 4);
-            SystemClock.sleep(7000);
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() + 4);
-            SystemClock.sleep(7000);
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() + 4);
-            SystemClock.sleep(7000);
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() - 4);
-            SystemClock.sleep(7000);
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() - 4);
-            SystemClock.sleep(7000);
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() - 4);
-            SystemClock.sleep(7000);
             t1.setTextSize(TypedValue.COMPLEX_UNIT_PX,t1.getTextSize() - 6);
-            SystemClock.sleep(7000);
 
         }
-    }
+    }*/
 
 }
