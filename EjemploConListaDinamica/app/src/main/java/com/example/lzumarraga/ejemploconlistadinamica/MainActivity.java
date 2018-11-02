f7b1f41d7ea56753e23cc75e2e450578a2ca8d89
Unknown <luiszumarraga95@gmail.com>
2018-11-02 12:37:54 +0100

    PAM

--- EjemploConListaDinamica/app/src/main/java/com/example/lzumarraga/ejemploconlistadinamica/MainActivity.java
+++ EjemploConListaDinamica/app/src/main/java/com/example/lzumarraga/ejemploconlistadinamica/MainActivity.java
@@ -13,11 +13,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class MainActivity extends ListActivity  {
+public class MainActivity extends ListActivity {
 
     Equipo[] equipos = new Equipo[25];
     TextView selection;
@@ -31,33 +32,33 @@ public class MainActivity extends ListActivity  {
         rellenarEquipos();
 
         setListAdapter(new IconicAdapter<Equipo>(this, R.layout.fila_equipo_primera, R.id.label, equipos));
-        selection=findViewById(R.id.selection);
+        selection = findViewById(R.id.selection);
 
     }
 
     /*
-    * El metodo rellena un array con varios objetos equipo de futbol para mostrarlos en una lista
-    * */
+     * El metodo rellena un array con varios objetos equipo de futbol para mostrarlos en una lista
+     * */
     public void rellenarEquipos() {
-        Equipo sevilla = new Equipo("sevilla","Sevilla FC", R.drawable.sevilla, liga.PRIMERA ,"El Sevilla Fútbol Club es un " +
+        Equipo sevilla = new Equipo("sevilla", "Sevilla FC", R.drawable.sevilla, liga.PRIMERA, "El Sevilla Fútbol Club es un " +
                 "club de fútbol español organizado como sociedad anónima deportiva. Tiene su sede en Sevilla, capital de la comunidad " +
                 "autónoma de Andalucía, y actualmente juega en Primera División. Fue fundado el 25 de enero de 18905\u200B6\u200B y " +
                 "su primer presidente fue el vice-cónsul británico Edward Farquharson Johnston.7\u200B Posteriormente fue inscrito " +
                 "en el registro de asociaciones el 14 de octubre de 1905, siendo su presidente el jerezano José Luis Gallegos Arnosa.");
-        Equipo betis = new Equipo("betis","Real Betis", R.drawable.betis, liga.PRIMERA, "El Real Betis Balompié, también conocido " +
+        Equipo betis = new Equipo("betis", "Real Betis", R.drawable.betis, liga.PRIMERA, "El Real Betis Balompié, también conocido " +
                 "como Real Betis o simplemente Betis, es una entidad polideportiva con sede en Sevilla, España. Fue establecida " +
                 "en septiembre de 1908 para la práctica del fútbol —aunque sus orígenes datan de 1907 y así consta como fecha " +
                 "fundacional por el propio club—, y oficialmente registrada el 1 de febrero de 1909.");
-        Equipo madrid = new Equipo("madrid","Real Madrid",R.drawable.madrid, liga.PRIMERA, "El Real Madrid Club de Fútbol, " +
+        Equipo madrid = new Equipo("madrid", "Real Madrid", R.drawable.madrid, liga.PRIMERA, "El Real Madrid Club de Fútbol, " +
                 "más conocido simplemente como Real Madrid, es una entidad polideportiva con sede en Madrid, España. Fue declarada " +
                 "oficialmente registrada como club de fútbol por sus socios el 6 de marzo de 1902 con el objeto de la práctica y " +
                 "desarrollo de este deporte —si bien sus orígenes datan al año 1900,8\u200B y su denominación de (Sociedad) Madrid " +
                 "Foot-ball Club a noviembre de 1901—. Tuvo a Julián Palacios y los hermanos Juan Padrós y Carlos Padrós como " +
                 "principales valedores de su creación.");
-        Equipo barsa = new Equipo("barcelona","Barcelona FC", R.drawable.barsa, liga.PRIMERA);
-        Equipo valencia = new Equipo("valencia","Valencia", R.drawable.valencia, liga.PRIMERA);
-        Equipo talavera = new Equipo("real sociedad","Real Sociedad", R.drawable.realsociedad, liga.PRIMERA);
-        Equipo recre = new Equipo("alaves","Alavés", R.drawable.alaves, liga.PRIMERA);
+        Equipo barsa = new Equipo("barcelona", "Barcelona FC", R.drawable.barsa, liga.PRIMERA);
+        Equipo valencia = new Equipo("valencia", "Valencia", R.drawable.valencia, liga.PRIMERA);
+        Equipo talavera = new Equipo("real sociedad", "Real Sociedad", R.drawable.realsociedad, liga.PRIMERA);
+        Equipo recre = new Equipo("alaves", "Alavés", R.drawable.alaves, liga.PRIMERA);
 
         equipos[0] = sevilla;
         equipos[1] = betis;
@@ -105,32 +106,73 @@ public class MainActivity extends ListActivity  {
         public View getView(int position, View convertView, ViewGroup parent) {
 
             View row = convertView;
+            ViewHolder holder;
+            TextView lab;
+            ImageView imgV;
 
             //DIBUJA MAL LAS FILAS CUANDO NO ES NULL, HAY QUE SOBREESCRIBIR METODOS
-            if (row==null){
-                LayoutInflater inflater=getLayoutInflater();
+            if (row == null) {
+                LayoutInflater inflater = getLayoutInflater();
 
-                if(equipos[position].getLiga()==liga.PRIMERA){
-                    row=inflater.inflate(R.layout.fila_equipo_primera, parent, false);
-                }else{
-                    if(equipos[position].getLiga()==liga.SEGUNDA){
-                        row=inflater.inflate(R.layout.fila_equipo_segunda, parent, false);
+                if (getItemViewType(position) == 0) {
+                    row = inflater.inflate(R.layout.fila_equipo_primera, parent, false);
+                } else {
+                    if (getItemViewType(position) == 1) {
+                        row = inflater.inflate(R.layout.fila_equipo_segunda, parent, false);
                     }
                 }
+
+                lab = (TextView) row.findViewById(R.id.label);
+                imgV = (ImageView) row.findViewById(R.id.icon);
+                holder = new ViewHolder(lab, imgV);
+                row.setTag(holder);
+
+            } else {
+                holder = (ViewHolder) row.getTag();
             }
 
-            TextView label = (TextView) row.findViewById(R.id.label);
-            label.setText(equipos[position].toString());
+            holder.getLab().setText(equipos[position].toString());
+            holder.getImgV().setImageResource(equipos[position].getEscudo());
 
-            icon = (ImageView)row.findViewById(R.id.icon);
-            icon.setImageResource(equipos[position].getEscudo());
-
-            return(row);
+            return (row);
         }
 
-//        public int getItemViewTyoe(){
-//
-//        }
+        @Override
+        public int getViewTypeCount() {
+            return 2;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            int type = -1;
+
+            if (equipos[position].getLiga() == liga.PRIMERA) {
+                type = 0;
+            } else {
+                if (equipos[position].getLiga() == liga.SEGUNDA) {
+                    type = 1;
+                }
+            }
+            return type;
+        }
+    }
+
+    class ViewHolder {
+        TextView lab;
+        ImageView imgV;
+
+        ViewHolder(TextView lab, ImageView imgV) {
+            this.lab = lab;
+            this.imgV = imgV;
+        }
+
+        public TextView getLab() {
+            return this.lab;
+        }
+
+        public ImageView getImgV() {
+            return this.imgV;
+        }
     }
 
 
@@ -146,7 +188,7 @@ public class MainActivity extends ListActivity  {
     /*
      * El metodo convierte una imageview a un array de bytes para poder mandarla en un intent
      * */
-    private byte[] convertirImagenABitmap (ImageView icono, int position){
+    private byte[] convertirImagenABitmap(ImageView icono, int position) {
 
         Bitmap bmp = BitmapFactory.decodeResource(getResources(), equipos[position].getEscudo());
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -155,4 +197,6 @@ public class MainActivity extends ListActivity  {
 
         return byteArray;
     }
+
+
 }
