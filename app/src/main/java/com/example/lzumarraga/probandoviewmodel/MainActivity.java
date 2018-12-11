b8ca05a6f1ad24eda40ea388492e965a498fae81
Unknown <luiszumarraga95@gmail.com>
2018-12-11 10:48:48 +0100

    clicker

--- app/src/main/java/com/example/lzumarraga/probandoviewmodel/MainActivity.java
+++ app/src/main/java/com/example/lzumarraga/probandoviewmodel/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.lzumarraga.probandoviewmodel;
 
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         miViewModel = ViewModelProviders.of(this).get(ViewModel.class);
+        displayForPuntos(miViewModel.contador);
     }
 
+    public void onClickSumar(View view){
+        miViewModel.sumatron();
+        displayForPuntos(miViewModel.contador);
+    }
+
+    public void onClickSumadorx2(View view){
+        miViewModel.sumadorx2();
+        displayForPuntos(miViewModel.contador);
+    }
+
+    public void onClickSumadorx4(View view){
+        miViewModel.sumadorx4();
+        displayForPuntos(miViewModel.contador);
+    }
+
+    public void displayForPuntos(long puntos){
+        TextView txtPuntos = findViewById(R.id.txtPuntos);
+        TextView txtSumador = findViewById(R.id.txtSumador);
+        TextView txtPreciox2 = findViewById(R.id.txtPreciox2);
+        TextView txtPreciox4 = findViewById(R.id.txtPreciox4);
+        txtPuntos.setText(String.valueOf(puntos));
+        txtSumador.setText(String.valueOf(miViewModel.sumador));
+        txtPreciox2.setText(String.valueOf(miViewModel.preciox2));
+        txtPreciox4.setText(String.valueOf(miViewModel.preciox4));
+    }
 
 }
--- app/src/main/java/com/example/lzumarraga/probandoviewmodel/ViewModel.java
+++ app/src/main/java/com/example/lzumarraga/probandoviewmodel/ViewModel.java
@@ -2,7 +2,32 @@ package com.example.lzumarraga.probandoviewmodel;
 
 public class ViewModel extends android.arch.lifecycle.ViewModel{
 
-    int multipicador = 1;
-    int contador = 0;
+    public long sumador = 1;
+    public long contador = 0;
+    public long preciox2 = 50;
+    public long preciox4 = 100;
+
+    public long sumatron (){
+        contador = contador + sumador;
+        return contador;
+    }
+
+    public void sumadorx2(){
+        if(contador >= preciox2){
+            sumador = sumador * 2;
+            contador = contador - preciox2;
+            preciox2 = preciox2 * 2;
+//            preciox4 = preciox4 * 2;
+        }
+    }
+
+    public void sumadorx4(){
+        if(contador >= preciox4) {
+            sumador = sumador * 4;
+            contador = contador - preciox4;
+//            preciox2 = preciox2 * 2;
+            preciox4 = preciox4 * 2;
+        }
+    }
 
 }
