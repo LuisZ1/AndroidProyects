d0795c139405cdeb2833cb216d70800536a6e920
Unknown <luiszumarraga95@gmail.com>
2018-12-11 09:35:57 +0100

    Probando ViewModel

---
+++ app/src/main/java/com/example/lzumarraga/probandoviewmodel/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lzumarraga.probandoviewmodel;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewModel miViewModel = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        miViewModel = ViewModelProviders.of(this).get(ViewModel.class);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/lzumarraga/probandoviewmodel/ViewModel.java
@@ -0,0 +1,8 @@
+package com.example.lzumarraga.probandoviewmodel;
+
+public class ViewModel extends android.arch.lifecycle.ViewModel{
+
+    int multipicador = 1;
+    int contador = 0;
+
+}
