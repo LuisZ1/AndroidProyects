7a886d229abafa6b9001a0915d24602ea422ecf1
lz <luiszumarraga95@gmail.com>
2018-12-13 14:13:51 +0100

    modificados multiplicadores Clicker

--- app/src/main/java/com/example/lzumarraga/probandoviewmodel/ViewModel.java
+++ app/src/main/java/com/example/lzumarraga/probandoviewmodel/ViewModel.java
@@ -2,21 +2,18 @@ package com.example.lzumarraga.probandoviewmodel;
 
 public class ViewModel extends android.arch.lifecycle.ViewModel{
 
-    public long sumador = 1;
-    public long contador = 0;
+    public long sumador = 1, contador = 0;
     public long precioCobre = 50;
     public long precioBronce = 5000;
     public long precioPlata = 500000;
-    public long x = 1;
-    public long contadorPulsaciones=0, contadorCobre = 0, contadorBronce =0;
+    public long x = 1, y = 0, z = 0;
+    public long contadorPulsaciones=0, contadorCobre = 0, contadorBronce = 0, contadorPlata = 0;
 
     public long sumatron (){
         contador = contador + sumador;
         contadorPulsaciones++;
         return contador;
     }
-
-    // 5 + 0.15x * 1.001^x
 
     public void sumadorCobre(){
         if(contador >= precioCobre) {
@@ -31,8 +28,8 @@ public class ViewModel extends android.arch.lifecycle.ViewModel{
         if(contador >= precioBronce) {
 
             contador = contador - precioBronce;
-            precioBronce = ((long)Math.ceil(precioBronce + Math.max(5000, 1550 * Math.pow(1.11, ++x))));
-            sumador = (long)Math.ceil(sumador + Math.max(100, 25 * Math.pow(1.07, x)));
+            precioBronce = ((long)Math.ceil(precioBronce + Math.max(5000, 1550 * Math.pow(1.11, ++y))));
+            sumador = (long)Math.ceil(sumador + Math.max(100, 25 * Math.pow(1.07, y)));
             contadorBronce++;
         }
     }
@@ -41,9 +38,9 @@ public class ViewModel extends android.arch.lifecycle.ViewModel{
     public void sumadorPlata(){
         if(contador >= precioPlata) {
             contador = contador - precioPlata;
-            precioPlata = ((long)Math.ceil(precioPlata + Math.max(5000, 1550 * Math.pow(1.11, ++x))));
-            sumador = (long)Math.ceil(sumador + Math.max(100, 25 * Math.pow(1.07, x)));
-            precioPlata++;
+            precioPlata = ((long)Math.ceil(precioPlata + Math.max(500000, 155000 * Math.pow(1.1111, ++z))));
+            sumador = (long)Math.ceil(sumador + Math.max(100, 2500 * Math.pow(1.07, z)));
+            contadorPlata++;
         }
     }
 
