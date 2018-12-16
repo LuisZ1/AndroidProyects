fad3a13da89dfe7102bd5e9975e8e400bee3acc8
Unknown <luiszumarraga95@gmail.com>
2018-12-16 17:49:27 +0100

    Añadido autoDisable de botones

--- ClickerFlama/app/src/main/java/com/luisz/flama/clicker/clickerflama/MainActivity.java
+++ ClickerFlama/app/src/main/java/com/luisz/flama/clicker/clickerflama/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,51 +17,70 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     ViewModel miViewModel = null;
+    Button btnBronce, btnCobre, btnPlata, btnOro, btnPlatino, btnDiamante;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         miViewModel = ViewModelProviders.of(this).get(ViewModel.class);
+        
+        btnCobre = (Button) findViewById(R.id.btnCobre);
+        btnBronce = (Button) findViewById(R.id.btnBronce);
+        btnPlata = (Button) findViewById(R.id.btnPlata);
+        btnOro = (Button) findViewById(R.id.btnOro);
+        btnPlatino = (Button) findViewById(R.id.btnPlatino);
+        btnDiamante = (Button) findViewById(R.id.btnDiamante);
+
         displayForPuntos(miViewModel.contador);
     }
 
     public void onClickSumar(View view){
         miViewModel.sumatron();
         displayForPuntos(miViewModel.contador);
+//        comprobarPrecios();
     }
 
     public void onClickSumadorCobre(View view){
         miViewModel.sumadorCobre();
         displayForPuntos(miViewModel.contador);
+//        comprobarPrecios();
     }
 
     public void onClickSumadorBronce(View view){
         miViewModel.sumadorBronce();
         displayForPuntos(miViewModel.contador);
+//        comprobarPrecios();
     }
 
     public void onClickSumadorPlata(View view){
         miViewModel.sumadorPlata();
         displayForPuntos(miViewModel.contador);
+//        comprobarPrecios();
     }
 
     public void onClickSumadorOro(View view){
         miViewModel.sumadorOro();
         displayForPuntos(miViewModel.contador);
+//        comprobarPrecios();
     }
 
     public void onClickSumadorPlatino(View view){
         miViewModel.sumadorPlatino();
         displayForPuntos(miViewModel.contador);
+//        comprobarPrecios();
     }
 
     public void onClickSumadorDiamante(View view){
         miViewModel.sumadorDiamante();
         displayForPuntos(miViewModel.contador);
+//        comprobarPrecios();
     }
 
     public void displayForPuntos(long puntos){
+
+        comprobarPrecios();
 
         DecimalFormat formatter = new DecimalFormat("###,###,###,###,###,###,###");
 
@@ -95,28 +115,54 @@ public class MainActivity extends AppCompatActivity {
         TextView txtContadorPulsaciones = findViewById(R.id.txtNPulsaciones);
         txtContadorPulsaciones.setText(formatter.format(miViewModel.contadorPulsaciones));
 
-//        TextView txtContadorParcial = findViewById(R.id.txtPulsacionesParcial);
-//        txtContadorParcial.setText(formatter.format(miViewModel.contadorPulsacionesParcial));
-//
-//        TextView txtContadorCobre = findViewById(R.id.txtPulsacionesCobre);
-//        txtContadorCobre.setText(String.valueOf(miViewModel.contadorCobre));
-//
-//        TextView txtContadorBronce = findViewById(R.id.txtPulsacionesBronce);
-//        txtContadorBronce.setText(String.valueOf(miViewModel.contadorBronce));
-//
-//        TextView txtContadorPlata = findViewById(R.id.txtPulsacionesPlata);
-//        txtContadorPlata.setText(String.valueOf(miViewModel.contadorPlata));
-
     }
 
+    public void comprobarPrecios(){
+        if(miViewModel.contador < miViewModel.precioCobre){
+            btnCobre.setEnabled(false);
+        }else{
+            btnCobre.setEnabled(true);
+        }
+
+        if(miViewModel.contador < miViewModel.precioBronce){
+            btnBronce.setEnabled(false);
+        }else{
+            btnBronce.setEnabled(true);
+        }
+
+        if(miViewModel.contador < miViewModel.precioPlata){
+            btnPlata.setEnabled(false);
+        }else{
+            btnPlata.setEnabled(true);
+        }
+
+        if(miViewModel.contador < miViewModel.precioOro){
+            btnOro.setEnabled(false);
+        }else{
+            btnOro.setEnabled(true);
+        }
+
+        if(miViewModel.contador < miViewModel.precioPlatino){
+            btnPlatino.setEnabled(false);
+        }else{
+            btnPlatino.setEnabled(true);
+        }
+
+        if(miViewModel.contador < miViewModel.precioDiamante){
+            btnDiamante.setEnabled(false);
+        }else{
+            btnDiamante.setEnabled(true);
+        }
+    }
 
     //MINERO AUTOMATICO
 
     private Timer timer;
     private TimerTask timerTask;
     private Handler handler = new Handler();
-    private int delay = 1000;
+    private int delay = 500;
     private int period = delay;
+
     public void stopTimer(){
         if(timer != null){
             timer.cancel();
@@ -138,7 +184,6 @@ public class MainActivity extends AppCompatActivity {
         };
         timer.schedule(timerTask, delay, period);
     }
-
 
     public void onSwitchClicked(View v){
         //Is the switch on?
