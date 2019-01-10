dccbca619e1b18471cbbd6f951f8e8bc24a8fa94
luisz <luiszumarraga95@gmail.com>
2019-01-10 23:04:57 +0100

    fragments v1

--- SimpleClicker/app/src/main/java/com/luisz/simpleclicker/MainActivity.java
+++ SimpleClicker/app/src/main/java/com/luisz/simpleclicker/MainActivity.java
@@ -1,15 +1,25 @@
 package com.luisz.simpleclicker;
 
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.luisz.simpleclicker.Fragments.HelpFragment;
+import com.luisz.simpleclicker.Fragments.HomeFragment;
+import com.luisz.simpleclicker.Fragments.SettingsFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+    private Typeface font;
 
     /*
     https://www.youtube.com/watch?v=zYVEMCiDcmY
@@ -24,11 +34,44 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        font = Typeface.createFromAsset(getAssets(), "awesome.ttf");
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.nav_drawer_open, R.string.nav_drawer_close);
         toggle.syncState();
 
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_inicio);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_inicio:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_ajustes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingsFragment()).commit();
+                break;
+            case R.id.nav_ayuda:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HelpFragment()).commit();
+                break;
+            case R.id.nav_mejoras:
+                Toast.makeText(this,"En construcción", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
@@ -39,4 +82,5 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
 }
