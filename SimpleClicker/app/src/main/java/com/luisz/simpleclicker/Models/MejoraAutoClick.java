bbc7dc19b6b4adf801830dcd8e39bff59c81e5b2
luisz <luiszumarraga95@gmail.com>
2019-01-14 22:56:26 +0100

    andrid

---
+++ SimpleClicker/app/src/main/java/com/luisz/simpleclicker/Models/MejoraAutoClick.java
@@ -0,0 +1,15 @@
+package com.luisz.simpleclicker.Models;
+
+public class MejoraAutoClick {
+
+    private int id;
+    private String nombre;
+    private long nivel;
+    private String colorFondo;
+
+    private long precioBase;
+    private long precio;
+    private long ingresosBase;
+    private long minimoSumador;
+
+}
