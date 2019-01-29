f2080c4de558bd0242e56ba83496135f468af4e4
Unknown <luiszumarraga95@gmail.com>
2019-01-29 10:19:06 +0100

    cambios en las listas

--- SimpleClicker/app/src/main/java/com/luisz/simpleclicker/Models/Constantes.java
+++ SimpleClicker/app/src/main/java/com/luisz/simpleclicker/Models/Constantes.java
@@ -2,53 +2,53 @@ package com.luisz.simpleclicker.Models;
 
 public class Constantes {
 
-    public static final double MULTIPLICADOR = 1.11;
-    public static final double DIVISOR_EXP_INICIAL = 1.7;
-    public static final double DIVISOR_EXP_MEDIO = 2;
-    public static final double DIVISOR_EXP_FINAL = 2.8;
+    public final double MULTIPLICADOR = 1.11;
+    public final double DIVISOR_EXP_INICIAL = 1.7;
+    public final double DIVISOR_EXP_MEDIO = 2;
+    public final double DIVISOR_EXP_FINAL = 2.8;
 
-    public static final long BASE_PRECIO_DIAMANTE   = 550000;
-    public static final long BASE_INGRESOS_DIAMANTE = 900;
+    public final long BASE_PRECIO_DIAMANTE   = 550000;
+    public final long BASE_INGRESOS_DIAMANTE = 900;
 
-    public static final long BASE_PRECIO_ALUMINIO   = 100;
-    public static final long BASE_INGRESOS_ALUMINIO = 5;
+    public final long BASE_PRECIO_ALUMINIO   = 100;
+    public final long BASE_INGRESOS_ALUMINIO = 5;
 
-    public static final long BASE_PRECIO_ZINC   = 2000;
-    public static final long BASE_INGRESOS_ZINC = 15;
+    public final long BASE_PRECIO_ZINC   = 2000;
+    public final long BASE_INGRESOS_ZINC = 15;
 
-    public static final long BASE_PRECIO_COBRE   = 6000;
-    public static final long BASE_INGRESOS_COBRE = 45;
+    public final long BASE_PRECIO_COBRE   = 6000;
+    public final long BASE_INGRESOS_COBRE = 45;
 
-    public static final long BASE_PRECIO_NIQUEL   = 12000;
-    public static final long BASE_INGRESOS_NIQUEL = 90;
+    public final long BASE_PRECIO_NIQUEL   = 12000;
+    public final long BASE_INGRESOS_NIQUEL = 90;
 
-    public static final long BASE_PRECIO_BRONCE   = 50000;
-    public static final long BASE_INGRESOS_BRONCE = 400;
+    public final long BASE_PRECIO_BRONCE   = 50000;
+    public final long BASE_INGRESOS_BRONCE = 400;
 
-    public static final long BASE_PRECIO_PLATA   = 550000;
-    public static final long BASE_INGRESOS_PLATA = 900;
+    public final long BASE_PRECIO_PLATA   = 550000;
+    public final long BASE_INGRESOS_PLATA = 900;
 
-    public static final long BASE_PRECIO_IRIDIO   = 3000000;
-    public static final long BASE_INGRESOS_IRIDIO = 2000;
+    public final long BASE_PRECIO_IRIDIO   = 3000000;
+    public final long BASE_INGRESOS_IRIDIO = 2000;
 
-    public static final long BASE_PRECIO_ORO   = 6500000;
-    public static final long BASE_INGRESOS_ORO = 4100;
+    public final long BASE_PRECIO_ORO   = 6500000;
+    public final long BASE_INGRESOS_ORO = 4100;
 
-    public static final long BASE_PRECIO_PLATINO   = 9000000;
-    public static final long BASE_INGRESOS_PLATINO = 7500;
+    public final long BASE_PRECIO_PLATINO   = 9000000;
+    public final long BASE_INGRESOS_PLATINO = 7500;
 
-    public static final long BASE_PRECIO_URANIO   = 15000000;
-    public static final long BASE_INGRESOS_URANIO = 10000;
+    public final long BASE_PRECIO_URANIO   = 15000000;
+    public final long BASE_INGRESOS_URANIO = 10000;
 
     //hierro, estaño
     //adamantio, tungsteno,
 
 
-    public static final int MINIMO_SUMADOR_COBRE    = 1;
-    public static final int MINIMO_SUMADOR_BRONCE   = 10;
-    public static final int MINIMO_SUMADOR_PLATA    = 100;
-    public static final int MINIMO_SUMADOR_ORO      = 1000;
-    public static final int MINIMO_SUMADOR_PLATINO  = 10000;
-    public static final int MINIMO_SUMADOR_DIAMANTE = 100000;
+    public final int MINIMO_SUMADOR_COBRE    = 1;
+    public final int MINIMO_SUMADOR_BRONCE   = 10;
+    public final int MINIMO_SUMADOR_PLATA    = 100;
+    public final int MINIMO_SUMADOR_ORO      = 1000;
+    public final int MINIMO_SUMADOR_PLATINO  = 10000;
+    public final int MINIMO_SUMADOR_DIAMANTE = 100000;
 
 }
--- SimpleClicker/app/src/main/java/com/luisz/simpleclicker/ViewModel/Util_Listas.java
+++ SimpleClicker/app/src/main/java/com/luisz/simpleclicker/ViewModel/Util_Listas.java
@@ -20,16 +20,16 @@ public class Util_Listas {
     public ArrayList<Mejora> rellenarListaMejoras(Context contexto){
         ArrayList<Mejora> listaMejoras = new ArrayList<Mejora>();
 
-        listaMejoras.add(new Mejora(1, contexto.getString(R.string.aluminio), 0, cons.BASE_PRECIO_ALUMINIO, cons.BASE_INGRESOS_ALUMINIO, cons.BASE_INGRESOS_ALUMINIO, "#D0D8D9"));
-        listaMejoras.add(new Mejora(2, contexto.getString(R.string.zinc), 0, cons.BASE_PRECIO_ZINC, cons.BASE_INGRESOS_ZINC, cons.BASE_INGRESOS_ZINC, "#B6B6B6"));
-        listaMejoras.add(new Mejora(3, contexto.getString(R.string.cobre), 0, cons.BASE_PRECIO_COBRE, cons.BASE_INGRESOS_COBRE, cons.BASE_INGRESOS_COBRE, "#FFB74D"));
-        listaMejoras.add(new Mejora(4, contexto.getString(R.string.niquel), 0, cons.BASE_PRECIO_NIQUEL, cons.BASE_INGRESOS_NIQUEL, cons.BASE_INGRESOS_NIQUEL, "#FFE57F"));
-        listaMejoras.add(new Mejora(5, contexto.getString(R.string.bronce), 0, cons.BASE_PRECIO_BRONCE, cons.BASE_INGRESOS_BRONCE, cons.BASE_INGRESOS_BRONCE, "#EF6C00"));
-        listaMejoras.add(new Mejora(6, contexto.getString(R.string.plata), 0, cons.BASE_PRECIO_PLATA, cons.BASE_INGRESOS_PLATA, cons.BASE_INGRESOS_PLATA, "#9E9E9E"));
-        listaMejoras.add(new Mejora(7, contexto.getString(R.string.iridio), 0, cons.BASE_PRECIO_IRIDIO, cons.BASE_INGRESOS_IRIDIO, cons.BASE_INGRESOS_IRIDIO, "#FFFF8D"));
-        listaMejoras.add(new Mejora(8, contexto.getString(R.string.oro), 0, cons.BASE_PRECIO_ORO, cons.BASE_INGRESOS_ORO, cons.BASE_INGRESOS_ORO, "#FFD600"));
-        listaMejoras.add(new Mejora(9, contexto.getString(R.string.platino), 0, cons.BASE_PRECIO_PLATINO, cons.BASE_INGRESOS_PLATINO, cons.BASE_INGRESOS_PLATINO, "#BDBDBD"));
-        listaMejoras.add(new Mejora(10, contexto.getString(R.string.uranio), 0, cons.BASE_PRECIO_URANIO, cons.BASE_INGRESOS_URANIO, cons.BASE_INGRESOS_URANIO, "#137656"));
+        listaMejoras.add(new Mejora(101, contexto.getString(R.string.aluminio), 0, cons.BASE_PRECIO_ALUMINIO, cons.BASE_INGRESOS_ALUMINIO, cons.BASE_INGRESOS_ALUMINIO, "#D0D8D9"));
+        listaMejoras.add(new Mejora(102, contexto.getString(R.string.zinc), 0, cons.BASE_PRECIO_ZINC, cons.BASE_INGRESOS_ZINC, cons.BASE_INGRESOS_ZINC, "#B6B6B6"));
+        listaMejoras.add(new Mejora(103, contexto.getString(R.string.cobre), 0, cons.BASE_PRECIO_COBRE, cons.BASE_INGRESOS_COBRE, cons.BASE_INGRESOS_COBRE, "#FFB74D"));
+        listaMejoras.add(new Mejora(104, contexto.getString(R.string.niquel), 0, cons.BASE_PRECIO_NIQUEL, cons.BASE_INGRESOS_NIQUEL, cons.BASE_INGRESOS_NIQUEL, "#FFE57F"));
+        listaMejoras.add(new Mejora(105, contexto.getString(R.string.bronce), 0, cons.BASE_PRECIO_BRONCE, cons.BASE_INGRESOS_BRONCE, cons.BASE_INGRESOS_BRONCE, "#EF6C00"));
+        listaMejoras.add(new Mejora(106, contexto.getString(R.string.plata), 0, cons.BASE_PRECIO_PLATA, cons.BASE_INGRESOS_PLATA, cons.BASE_INGRESOS_PLATA, "#9E9E9E"));
+        listaMejoras.add(new Mejora(107, contexto.getString(R.string.iridio), 0, cons.BASE_PRECIO_IRIDIO, cons.BASE_INGRESOS_IRIDIO, cons.BASE_INGRESOS_IRIDIO, "#FFFF8D"));
+        listaMejoras.add(new Mejora(108, contexto.getString(R.string.oro), 0, cons.BASE_PRECIO_ORO, cons.BASE_INGRESOS_ORO, cons.BASE_INGRESOS_ORO, "#FFD600"));
+        listaMejoras.add(new Mejora(109, contexto.getString(R.string.platino), 0, cons.BASE_PRECIO_PLATINO, cons.BASE_INGRESOS_PLATINO, cons.BASE_INGRESOS_PLATINO, "#BDBDBD"));
+        listaMejoras.add(new Mejora(110, contexto.getString(R.string.uranio), 0, cons.BASE_PRECIO_URANIO, cons.BASE_INGRESOS_URANIO, cons.BASE_INGRESOS_URANIO, "#137656"));
 
         return listaMejoras;
     }
@@ -40,10 +40,10 @@ public class Util_Listas {
     public ArrayList<Mejora_AutoClick> rellenarListaMejorasAutoClick(Context contexto){
         ArrayList<Mejora_AutoClick> listaMejorasAutoClick = new ArrayList<Mejora_AutoClick>();
 
-        listaMejorasAutoClick.add(new Mejora_AutoClick(1, "Nivel 1", 10000000, 1000, "#90CAF9"));
-        listaMejorasAutoClick.add(new Mejora_AutoClick(2, "Nivel 2", 100000000, 500, "#42A5F5"));
-        listaMejorasAutoClick.add(new Mejora_AutoClick(3, "Nivel 3", 1000000000, 100, "#1E88E5"));
-        listaMejorasAutoClick.add(new Mejora_AutoClick(4, "Nivel 4", 10000000000L, 50, "#1565C0"));
+        listaMejorasAutoClick.add(new Mejora_AutoClick(201, "Nivel 1", 10000000, 1000, "#90CAF9"));
+        listaMejorasAutoClick.add(new Mejora_AutoClick(202, "Nivel 2", 100000000, 500, "#42A5F5"));
+        listaMejorasAutoClick.add(new Mejora_AutoClick(203, "Nivel 3", 1000000000, 100, "#1E88E5"));
+        listaMejorasAutoClick.add(new Mejora_AutoClick(204, "Nivel 4", 10000000000L, 50, "#1565C0"));
 
         return listaMejorasAutoClick;
     }
@@ -54,10 +54,10 @@ public class Util_Listas {
     public ArrayList<Mejora_Per_Maq_Her> rellenarListaMejorasPersonal(Context contexto){
         ArrayList<Mejora_Per_Maq_Her> listaMejorasPersonal = new ArrayList<Mejora_Per_Maq_Her>();
 
-        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(1,"Peón",30000000,0.5,70,"#FFC107",R.drawable.it_peon));
-        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(2,"Minero",55000000,2,20,"#FFA000",R.drawable.it_minero));
-        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(3,"Arquitecto",1000000000,3,10,"#FF6F00", R.drawable.it_arquitecto));
-        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(4,"Jefazo",15000000000L,8,5,"#E65100", R.drawable.it_jefazo));
+        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(301,"Peón",30000000,0.5,70,"#FFC107",R.drawable.it_peon));
+        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(302,"Minero",55000000,2,20,"#FFA000",R.drawable.it_minero));
+        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(303,"Arquitecto",1000000000,3,10,"#FF6F00", R.drawable.it_arquitecto));
+        listaMejorasPersonal.add(new Mejora_Per_Maq_Her(304,"Jefazo",15000000000L,8,5,"#E65100", R.drawable.it_jefazo));
 
         return listaMejorasPersonal;
     }
@@ -69,11 +69,11 @@ public class Util_Listas {
     public ArrayList<Mejora_Per_Maq_Her> rellenarListaMejorasMaquinaria(Context contexto){
         ArrayList<Mejora_Per_Maq_Her> listaMejorasMaquinaria = new ArrayList<Mejora_Per_Maq_Her>();
 
-        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(1,"Pico",1000000,0.15,75,"#90A4AE", R.drawable.it_pico));
-        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(2,"Martillo neumático",350000000,1.5,20,"#607D8B", R.drawable.it_pico));
-        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(3,"Vagoneta",750000000,3,3,"#455A64", R.drawable.it_vagon));
-        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(4,"Tuneladora",1500000000,8.5,2,"#263238", R.drawable.it_tuneladora));
-        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(5,"Dinamita",3000000000L,35,1,"#d50000", R.drawable.it_pico));
+        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(401,"Pico",1000000,0.15,75,"#90A4AE", R.drawable.it_pico));
+        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(402,"Martillo neumático",350000000,1.5,20,"#607D8B", R.drawable.it_pico));
+        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(403,"Vagoneta",750000000,3,3,"#455A64", R.drawable.it_vagon));
+        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(404,"Tuneladora",1500000000,8.5,2,"#263238", R.drawable.it_tuneladora));
+        listaMejorasMaquinaria.add(new Mejora_Per_Maq_Her(405,"Dinamita",3000000000L,35,1,"#d50000", R.drawable.it_pico));
 
         return listaMejorasMaquinaria;
     }
@@ -84,11 +84,11 @@ public class Util_Listas {
     public ArrayList<Mejora_Per_Maq_Her> rellenarListaMejorasHerramientas(Context contexto){
         ArrayList<Mejora_Per_Maq_Her> listaMejorasHerramientas = new ArrayList<Mejora_Per_Maq_Her>();
 
-        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(1,"Madera",1000000,2,2,"#795548", R.drawable.it_piedra));
-        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(2,"Piedra",25000000,5,2,"#B0BEC5", R.drawable.it_piedra));
-        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(3,"Hierro",400000000,10,1,"#78909C", R.drawable.it_piedra));
-        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(4,"Oro",5500000000L,15,1,"#FFD600", R.drawable.it_piedra));
-        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(5,"Diamante",20000000000L,20,1,"#00BCD4", R.drawable.it_piedra));
+        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(501,"Madera",1000000,2,2,"#795548", R.drawable.it_piedra));
+        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(502,"Piedra",25000000,5,2,"#B0BEC5", R.drawable.it_piedra));
+        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(503,"Hierro",400000000,10,1,"#78909C", R.drawable.it_piedra));
+        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(504,"Oro",5500000000L,15,1,"#FFD600", R.drawable.it_piedra));
+        listaMejorasHerramientas.add(new Mejora_Per_Maq_Her(505,"Diamante",20000000000L,20,1,"#00BCD4", R.drawable.it_piedra));
 
         return listaMejorasHerramientas;
     }
